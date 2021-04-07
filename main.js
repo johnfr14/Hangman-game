ec857a3f7849cf6e2e5a1be5f196b07f9e8e07f3
johnfr14 <jonathan.alyra@gmail.com>
2021-04-07 09:25:39 +0200

    commit

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ const fs = require('fs')
 const readline = require('readline-sync')
 const { randomInt } = require('crypto')
 const { HANGMANPICS } = require('./template-hangman')
+const { Joueur } = require('./joueur')
+const chalk = require('chalk')
 
-while(true){
+do{
   //--------------------------------INTRO-----------------------------------------//
   // initialisation of the mystery word 
   let dico = fs.readFileSync('./dictionnaire', 'utf-8').toLowerCase().split('\n')                     // we recupe all the words in our dictionnary and store them in a array 
@@ -15,18 +17,18 @@ while(true){
     wordToFind.push('_')                                                                               //
   }
 
-  console.log('BOONjour BONjour tout le monde et bienvenue dans le jeu du PENDU GAME !!!!!!!')
+  console.log(chalk.bold('BOONjour BONjour tout le monde et bienvenue dans le jeu du PENDU GAME !!!!!!!'))
 
   // Getting the player's data 
   console.log('\nVeuillez dans un premier temps nous donner votre joli petit nom\n(entre 1 et 20 charactères svp faite pas le forceur)\n')
 
   let life = 7
-  let joueurId = readline.question('votre nom: ')
+  let joueurId = readline.question(chalk.yellow('votre nom: '))
   while (joueurId.length < 1 || joueurId.length > 20) {
-    joueurId = readline.question('\nNope ! entre 1 et 20 charactères\nvotre nom: ')
+    joueurId = readline.question(`\nNope ! entre 1 et 20 charactères\n${chalk.yellow('votre nom: ')}`)
   }
   
-  console.log(`\n\nBien Bien Bien ${joueurId}, tu as maintenant ${life} vies (pas comme les chats) pour trouver le mot très mystère !
+  console.log(`\n\nBien Bien Bien ${chalk.yellow(joueurId)}, tu as maintenant ${chalk.green(life)} vies (pas comme les chats) pour trouver le mot très mystère !
 Si tu fais ${life} erreurs, tu seras...... MORT !!!!!\n\nà toi de jouer`)
 
 
@@ -39,16 +41,16 @@ Si tu fais ${life} erreurs, tu seras...... MORT !!!!!\n\nà toi de jouer`)
   let h = 0
   while (life && wordToFind.includes('_')) {
     // displaying the hanging
-    console.log(HANGMANPICS[h])
+    console.log(chalk.blackBright(HANGMANPICS[h]))
 
     // Player choose a letter (very stressful !!!!)
     let letter = ''
-    console.log(`Le mot : ${wordToFind.join(' ')}\t\tvie restant : ${life}`)
+    console.log(`Le mot : ${wordToFind.join(' ')}\t\tvie restant : ${life > 5 ? chalk.green(life) : life < 3 ? chalk.red(life) : chalk.yellow(life)}`)
     console.log(`Lettre déjà utilisé : ${wordAlreadyUsed}\n`)
 
     letter = readline.question('Votre lettre : ').toLowerCase()
     while (letter < 'a' || letter > 'z' || letter.length !== 1) {
-      letter = readline.question("NOPE ! ne choisissez qu\'une lettre entre 'a' et 'z' !!!!\nVotre lettre : ").toLowerCase()
+      letter = readline.question(`NOPE ! ne choisissez qu\'une lettre entre 'a' et 'z' !!!!\n${chalk.yellow('Votre lettre : ')}`).toLowerCase()
     }
 
     // check if the letter match ones of the mystery word
@@ -79,15 +81,11 @@ Si tu fais ${life} erreurs, tu seras...... MORT !!!!!\n\nà toi de jouer`)
     console.log('')
   }
 
-  if (life === 0) {
-    console.log('GAME OVER !')
-    console.log(`Le mot était : ${mysteryWord.join('')}\n\n`)
-    let choice = readline.question('Voulez-vous rejouer ?\nYes [y]\nNo [n]\nVotre choix : ')
-    choice === 'y' ? '' : process.exit(1)
-  } else {
-    console.log('YOU WIN !!!')
+  if (life) {
+    console.log(chalk.green('YOU WIN !!!'))
     console.log(`Le mot était bien : ${mysteryWord.join('')} !!!!!\n\n`)
-    let choice = readline.question('Voulez-vous rejouer ?\nYes [y]\nNo [n]\nVotre choix : ')
-    choice === 'y' ? '' : process.exit(1)
+  } else {
+    console.log(chalk.red('GAME OVER !'))
+    console.log(`Le mot était : ${mysteryWord.join('')}\n\n`)
   }
-}
+} while (readline.keyInYN('Voulez-vous rejouer ?'))
